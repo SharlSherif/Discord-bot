b5ed6e21a8437a480b0d808578bc7b350f2b8122
Charlie <ssharl@ymail.com>
2018-03-17 07:28:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,10 @@ app.use('/users', users);
 
 
 
-
+var http = require("http");
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 bot.login(process.env.BOT_TOKEN);
 

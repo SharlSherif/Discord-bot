adcfc87d640ef0a68ca642ef997f1f7675bc3801
Charlie <ssharl@ymail.com>
2018-03-18 12:44:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const prefix = "!!";
       let oldStatus     = oldMember.user.presence.status;
       let guildChannels = newMember.guild.channels;
 
-        if(newMember.user.bot == false && status == "online" && newMember.user.username !=='TheGermanGuy' && oldStatus == "offline"){ // to exclude bots, and take action only if user status is {Online}
+        if(newMember.user.bot == false && status == "online" && newMember.user.username !=='TheGermanGuy'){ // to exclude bots, and take action only if user status is {Online}
             guildChannels.find('name','nipponchan').send(`${newMember.user.username} is now ${status}`,{tts:true}).catch((err)=>send(err));
             console.log(`${newMember.user.username} is now ${status}`);
         }else {
@@ -63,55 +63,4 @@ const prefix = "!!";
       });
 
 
-  bot.on('message', (message) => {
-
-    if (message.content.startsWith(prefix + "creator")) {
-
-      let botembed = new Discord.RichEmbed()
-      .setDescription("Creator info")
-      .setColor("#41caf4")
-      .addField('.............................................',`I was made by the greatest man alive, ANUBIS`);
-
-      message.channel.send(botembed);
-    }else if (message.content.startsWith(prefix + "secret")){
-
-      message.channel.send('magnus and luca are retarded', {tts:true});
-
-    }else if (message.content.startsWith(prefix + "delete channel")){
-      let deleting = new Discord.RichEmbed()
-      .setDescription("everything is being deleted...")
-      .setColor("#41caf4")
-      .addField('.............................................',`faggot`);
-
-      message.channel.send(deleting);
-    }
-  });
-
-bot.on('disconnect', function(msg, code) {
-    if (code === 0) return console.error(msg);
-    bot.connect();
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-app.listen(port, ()=>{
-  console.log(`server is up on ${port}`);
-})
-module.exports = app;
+  
